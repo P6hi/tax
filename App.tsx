95a84007c3cc84d818210b807a07abd2a8470b64
Arthur <a-murk@mail.ee>
2022-09-10 17:17:52 +0300

    Create monthy wage calculator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React , { useState } from 'react';
 
 export default function App() {
+
+  const [brutto, setBrutto] = useState(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Eesti palgakalkulaator</Text>
+      </View>
+    <View style={[styles.container, styles.bottomContainer]}>
+      <Text style={styles.text}>Netokuupalk:</Text>
+      <TextInput style={styles.input} keyboardType='numeric' onChangeText={val => setBrutto(parseInt(val))}></TextInput>
+      <Result brutto={brutto} />
       <StatusBar style="auto" />
     </View>
+    </View>
   );
+}
+
+const Result = ({brutto}: {brutto: number}) => {
+  if (brutto) {
+    return (
+      <Text>Brutokuupalk: {Math.round(brutto * 0.80)}€</Text>
+    )
+  }
+  return (
+    <Text>Ootan...</Text>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b6c5d6',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
+  input: {
+    backgroundColor: 'white',
+    width: '80%',
+    borderRadius: 10,
+    padding: 5,
+    margin: 10,
+  },
+  text: {
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  bottomContainer: {
+    justifyContent: 'flex-start',
+  }
 });
